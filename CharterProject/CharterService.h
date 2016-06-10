82bb2e841a25d8f53bd345897bdacc68756cc047
James Rochabrun <jamesrochabrun@mbp-de-james.attlocal.net>
2016-06-10 15:10:00 -0400

    model created

--- CharterProject/CharterService.h
+++ CharterProject/CharterService.h
@@ -9,12 +9,14 @@
 #import <Foundation/Foundation.h>
 
 @interface CharterService : NSObject
+
+@property NSString *name;
 @property NSNumber *advertisedPrice;
 @property NSArray *bookingFields;
 @property NSString *bookingMode;
 @property BOOL charter;
 @property NSString *confirmMode;
-@property NSString *confirmModeMinParticipants;
+@property NSNumber *confirmModeMinParticipants;
 @property NSString *currency;
 @property NSString *dateUpdated;
 @property NSString *charterDescription;
@@ -27,19 +29,21 @@
 @property NSString *longitude;
 @property NSDictionary *locationAddress;
 @property NSNumber *minimumNoticeMinutes;
-@property NSString *name;
 @property NSArray *priceOptions;
 @property NSString *productCode;
 @property NSString *productType;
 @property NSNumber *quantityRequired;
 @property NSNumber *quantityRequiredMax;
 @property NSNumber *quantityRequiredMin;
-@property NSString *shortDescription ;
+@property NSString *shortDescription;
 @property NSString *supplierAlias;
 @property NSNumber *supplierId;
 @property NSString *terms;
 @property NSString *unitLabel;
 @property NSString *unitLabelPlural;
+
+- (instancetype)initWithName:(NSString*)name advertisedPrice:(NSNumber*)advertisedPrice bookingFields:(NSArray*)bookingFields bookingMode:(NSString*)bookingMode charter:(BOOL)charter confirmMode:(NSString*)confirmMode confirmModeMinParticipants:(NSNumber*)confirmModeMinParticipants currency:(NSString*)currency dateUpdated:(NSString*)dateUpdated charterDescription:(NSString*)charterDescription durationMinutes:(NSNumber*)durationMinutes extras:(NSArray*)extras generalTerms:(NSString*)generalTerms images:(NSArray*)images internalCode:(NSString*)internalCode latitude:(NSString*)latitude longitude:(NSString*)longitude locationAddress:(NSDictionary*)locationAddress minimumNoticeMinutes:(NSNumber*)minimumNoticeMinutes priceOptions:(NSArray*)priceOptions productCode:(NSString*)productCode productType:(NSString*)productType quantityRequired:(NSNumber*)quantityRequired quantityRequiredMax:(NSNumber*)quantityRequiredMax quantityRequiredMin:(NSString*)quantityRequiredMin shortDescription:(NSString*)shortDescription supplierAlias:(NSString*)supplierAlias supplierId:(NSNumber*)supplierId terms:(NSString*)terms unitLabel:(NSString*)unitLabel unitLabelPlural:(NSString*)unitLabelPlural;
+
 
 
 @end
--- CharterProject/CharterService.m
+++ CharterProject/CharterService.m
@@ -9,5 +9,46 @@
 #import "CharterService.h"
 
 @implementation CharterService
+- (instancetype)initWithName:(NSString*)name advertisedPrice:(NSNumber*)advertisedPrice bookingFields:(NSArray*)bookingFields bookingMode:(NSString*)bookingMode charter:(BOOL)charter confirmMode:(NSString*)confirmMode confirmModeMinParticipants:(NSNumber*)confirmModeMinParticipants currency:(NSString*)currency dateUpdated:(NSString*)dateUpdated charterDescription:(NSString*)charterDescription durationMinutes:(NSNumber*)durationMinutes extras:(NSArray*)extras generalTerms:(NSString*)generalTerms images:(NSArray*)images internalCode:(NSString*)internalCode latitude:(NSString*)latitude longitude:(NSString*)longitude locationAddress:(NSDictionary*)locationAddress minimumNoticeMinutes:(NSNumber*)minimumNoticeMinutes priceOptions:(NSArray*)priceOptions productCode:(NSString*)productCode productType:(NSString*)productType quantityRequired:(NSNumber*)quantityRequired quantityRequiredMax:(NSNumber*)quantityRequiredMax quantityRequiredMin:(NSNumber*)quantityRequiredMin shortDescription:(NSString*)shortDescription supplierAlias:(NSString*)supplierAlias supplierId:(NSNumber*)supplierId terms:(NSString*)terms unitLabel:(NSString*)unitLabel unitLabelPlural:(NSString*)unitLabelPlural{
+    
+    self = [super init];
+    if (self) {
+        self.name = name;
+        self.advertisedPrice = advertisedPrice;
+        self.bookingFields = bookingFields;
+        self.bookingMode = bookingMode;
+        self.charter = charter;
+        self.confirmMode = confirmMode;
+        self.confirmModeMinParticipants = confirmModeMinParticipants;
+        self.currency = currency;
+        self.dateUpdated = dateUpdated;
+        self.charterDescription = charterDescription;
+        self.durationMinutes = durationMinutes;
+        self.extras = extras;
+        self.generalTerms = generalTerms;
+        self.images = images;
+        self.internalCode = internalCode;
+        self.latitude = latitude;
+        self.longitude = longitude;
+        self.locationAddress = locationAddress;
+        self.minimumNoticeMinutes = minimumNoticeMinutes;
+        self.priceOptions = priceOptions;
+        self.productCode = productCode;
+        self.productType = productType;
+        self.quantityRequired = quantityRequired;
+        self.quantityRequiredMax = quantityRequiredMax;
+        self.quantityRequiredMin = quantityRequiredMin;
+        self.shortDescription = shortDescription;
+        self.supplierAlias = supplierAlias;
+        self.supplierId = supplierId;
+        self.terms = terms;
+        self.unitLabel = unitLabel;
+        self.unitLabelPlural = unitLabelPlural;
+    }
+    return self;
+   
+}
+
+
 
 @end
