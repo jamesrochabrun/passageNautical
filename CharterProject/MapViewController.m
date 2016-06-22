544c3e882783f8cce972ff69ed98a483d233c6ea
james <jamesrochabrun@gmail.com>
2016-06-21 21:30:12 -0400

    mapview done

--- CharterProject/MapViewController.m
+++ CharterProject/MapViewController.m
@@ -11,11 +11,11 @@
 #import <CoreLocation/CoreLocation.h>
 #import "CharterService.h"
 
-@interface MapViewController ()<MKMapViewDelegate, CLLocationManagerDelegate>
+@interface MapViewController ()<MKMapViewDelegate,CLLocationManagerDelegate>
 @property CLLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
-
+@property (weak, nonatomic) IBOutlet UIButton *dismissButton;
+@property (weak, nonatomic) IBOutlet UIButton *getDirectionsButton;
 
 @end
 
@@ -25,8 +25,8 @@
 
 - (void)viewDidLoad {
     
-    NSLog(@"this %@", self.charterService.name);
-    
+    self.dismissButton.tintColor = [UIColor whiteColor];
+
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     [self.locationManager requestWhenInUseAuthorization];
@@ -34,18 +34,46 @@
     self.mapView.showsUserLocation = YES;
 }
 
+- (void)viewDidDisappear:(BOOL)animated {
+    [self.locationManager stopUpdatingLocation];
+}
+
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
-    
-    [self.mapView setRegion:MKCoordinateRegionMake(userLocation.coordinate, MKCoordinateSpanMake(0.5f, 0.5f)) animated:YES];
     
     MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
     point.coordinate = CLLocationCoordinate2DMake([self.charterService.latitude doubleValue], [self.charterService.longitude doubleValue]);
     point.title = @"Where am I?";
     point.subtitle = @"I'm here!!!";
     
-    [self.mapView addAnnotation:point];
+    [self.mapView setRegion:MKCoordinateRegionMake(point.coordinate, MKCoordinateSpanMake(0.5f, 0.5f)) animated:YES];
 
+    [self.mapView addAnnotation:point];
 }
+
+
+- (IBAction)switchMapTypes:(UISegmentedControl *)sender {
+    
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.mapView.mapType = MKMapTypeStandard;
+            break;
+        case 1:
+            self.mapView.mapType = MKMapTypeSatellite;
+            break;
+        case 2:
+            self.mapView.mapType = MKMapTypeHybrid;
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (IBAction)getDirectionsButtonTapped:(UIButton *)sender {
+    NSString *stringUrl = [NSString stringWithFormat:@"http://maps.apple.com/maps?daddr=%@,%@", self.charterService.latitude,self.charterService.longitude];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:stringUrl]];
+}
+
 
 
 - (IBAction)dismissView:(UIButton *)sender {
